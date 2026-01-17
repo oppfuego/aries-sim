a621819e5a63452043b834ccd1c5f7c16fe8ac2c
oppfuego <zaharenkoila19@gmail.com>
2026-01-17 20:59:59 +0200

    new logo

--- src/backend/services/auth.service.ts
+++ src/backend/services/auth.service.ts
@@ -28,8 +28,8 @@ export const authService = {
         const result = await this.issueTokensAndSession(user._id, user.email, user.role, undefined, undefined);
         await sendEmail(
             user.email,
-            "Welcome to Jetreex 🎉",
-            `Hi ${user.name}, thanks for registering at Jetreex.`
+            "Welcome to Aries Sim 🎉",
+            `Hi ${user.name}, thanks for registering at Aries Sim.`
         );
 
         return { user, ...result };
